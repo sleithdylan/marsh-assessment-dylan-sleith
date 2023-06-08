28cf974b80357ecc27a490be367ecb51dfaa80f2
Dylan Sleith <sleithdylan@gmail.com>
2023-06-08 22:48:56 +0100

    feat: add UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WeatherInfoComponent } from './components/weather-info/weather-info.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, WeatherInfoComponent, NavbarComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/weather-info/weather-info.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-info',
+  templateUrl: './weather-info.component.html',
+})
+export class WeatherInfoComponent {}
